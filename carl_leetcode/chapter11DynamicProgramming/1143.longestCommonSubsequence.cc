0439c53ab4cdf189f6d58a0db44f51f7fdd694e4
cs-cshi <572081809@qq.com>
2022-04-10 09:18:04 +0800

    leetcode

---
+++ carl_leetcode/chapter11DynamicProgramming/1143.longestCommonSubsequence.cc
@@ -0,0 +1,39 @@
+/**
+ * 1143. 最长公共子序列
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ *     例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ * 链接：https://leetcode-cn.com/problems/longest-common-subsequence
+ */
+
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using std::max;
+using std::string;
+using std::vector;
+
+class Solution
+{
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        /**
+         * dp[i][j] 长度为[0,i-1]和长度为[0,j-1] 中的最长公共子序列
+         */
+        vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1, 0));
+        for (int i = 1; i <= text1.size(); i++)
+        {
+            for (int j = 1; j <= text2.size(); j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[text1.size()][text2.size()];
+    }
+};
