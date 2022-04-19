14690b53e52efe191489f574ab9cd296a784cfba
cs-cshi <572081809@qq.com>
2022-04-19 10:19:41 +0800

    '0419'

---
+++ carl_leetcode/chapter6String/BM85.verifyIpAdress.cc
@@ -0,0 +1,28 @@
+/**
+ * BM85 验证IP地址
+ * 编写一个函数来验证输入的字符串是否是有效的 IPv4 或 IPv6 地址
+ * IPv4 地址由十进制数和点来表示，每个地址包含4个十进制数，其范围为 0 - 255， 用(".")分割。比如，172.16.254.1；
+ * 同时，IPv4 地址内的数不会以 0 开头。比如，地址 172.16.254.01 是不合法的。
+ * IPv6 地址由8组16进制的数字来表示，每组表示 16 比特。这些组数字通过 (":")分割。比如,  2001:0db8:85a3:0000:0000:8a2e:0370:7334 是一个有效的地址。而且，我们可以加入一些以 0 开头的数字，字母可以使用大写，也可以是小写。所以， 2001:db8:85a3:0:0:8A2E:0370:7334 也是一个有效的 IPv6 address地址 (即，忽略 0 开头，忽略大小写)。
+ * 然而，我们不能因为某个组的值为 0，而使用一个空的组，以至于出现 (::) 的情况。 比如， 2001:0db8:85a3::8A2E:0370:7334 是无效的 IPv6 地址。
+ * 同时，在 IPv6 地址中，多余的 0 也是不被允许的。比如， 02001:0db8:85a3:0000:0000:8a2e:0370:7334 是无效的。
+ * 链接：https://www.nowcoder.com/practice/55fb3c68d08d46119f76ae2df7566880?tpId=295&tqId=1024725&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj%3Ftab%3D%25E7%25AE%2597%25E6%25B3%2595%25E7%25AF%2587%26topicId%3D295
+ */
+
+#include <string>
+
+using std::string;
+
+class Solution
+{
+public:
+    /**
+     * 验证IP地址
+     * @param IP string字符串 一个IP地址字符串
+     * @return string字符串:IPv4,   IPv6,   Neither
+     */
+    string solve(string IP)
+    {
+        // write code here
+    }
+};
